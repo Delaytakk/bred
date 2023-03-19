6d652e8610d47e65b38ab4afcc603aac495c698e
Delaytakk <125904777+Delaytakk@users.noreply.github.com>
2023-03-19 18:51:53 +0300

    2

--- app/src/main/java/com/example/bred/SecondActivity.java
+++ app/src/main/java/com/example/bred/SecondActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Scanner;
+
 public class SecondActivity extends AppCompatActivity {
 
     @Override
@@ -13,8 +15,23 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
     }
-    public void startNotesActivity(View V){
+
+    public void startNotesActivity(View V) {
         Intent intent = new Intent(this, NotesActivity.class);
         startActivity(intent);
     }
-}
+
+
+    /*public static void  main(String[] listPlans){
+        Scanner console = new Scanner(System.in);
+        int size = 500;
+        String listPlans[] = new String[size];
+        for (int i = 0; i < size; i++) {
+            listPlans[i] = console.nextLine();
+            }
+        for (int i = 0; i < size; i++){
+            System.out.println(" " + listPlans[i]);
+            }
+        } */
+    }
+
